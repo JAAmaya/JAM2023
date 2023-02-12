05f2cab56fcea81f90a46495aff0aeb8e37a76ee
alealonso012 <64079719+alealonso012@users.noreply.github.com>
2023-02-12 20:09:13 +0100

    audio manager creado y sonidos subidos

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,69 @@
+using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager Instance;
+
+    public int SceneMusic = 0;
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        switch (SceneMusic)
+        {
+            case 0:
+                Play("TemaMenus");
+                break;
+            case 1:
+                Play("TemaPeleaBosque");
+                break;
+            case 2:
+                Play("TemaPeleaAgua");
+                break;
+            case 3:
+                Play("TemaPeleaMontaña");
+                break;
+            case 4:
+                Play("TemaPeleaDungeon");
+                break;
+        }
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+
+    public void PlayAttackSound()
+    {
+        AudioManager.Instance.Play("AtaqueAliado");
+    }
+}
