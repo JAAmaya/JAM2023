6c3602bc3ad17faf6f1e11c7d395b9ed53e11597
JorgeCambero <j.cambero.2019@alumnos.urjc.es>
2023-02-15 14:59:59 +0100

    Añadido el menu del libro

---
+++ Assets/PagLibro.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PagLibro : MonoBehaviour
+{
+    static int pagina = 0;
+    public Image libro;
+    public int maxPag;
+    private bool habilitado = false;
+    public GameObject handBook;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && handBook.activeInHierarchy)
+        {
+            habilitado = !habilitado;
+            FindObjectOfType<AudioManager>().Play("Inventario");
+            this.transform.GetChild(0).gameObject.SetActive(habilitado);
+            if (habilitado)
+            {
+                Cursor.lockState = CursorLockMode.Confined;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+            }
+        }
+    }
+    public void NextPage() 
+    {
+        if (pagina < maxPag) 
+        {
+            libro.transform.GetChild(pagina).gameObject.SetActive(false);
+            libro.transform.GetChild(pagina + 1).gameObject.SetActive(true);
+            pagina += 1;
+        }
+        
+    }
+    public void PrevPage()
+    {
+        if (pagina > 0)
+        {
+            libro.transform.GetChild(pagina).gameObject.SetActive(false);
+            libro.transform.GetChild(pagina - 1).gameObject.SetActive(true);
+            pagina -= 1;
+        }
+
+    }
+}
